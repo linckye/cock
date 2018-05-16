6e39bca6348afb07779e717be08b2ca814770a72
tianyoulin <tianyoulin@clubfactory.com>
2018-05-16 13:29:57 +0800

    init

---
+++ core/src/main/java/org/liquid/core/Function.java
@@ -0,0 +1,12 @@
+package org.liquid.core;
+
+/**
+ * @author linckye 2018-05-16
+ */
+public class Function {
+
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+    
+}
--- service/src/main/java/org/liquid/service/SchedulerService.java
+++
@@ -1,7 +0,0 @@
-package org.liquid.service;
-
-/**
- * @author linckye 2018-04-09
- */
-public interface SchedulerService {
-}
