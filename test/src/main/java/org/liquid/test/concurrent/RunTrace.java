152f0c0c37739540239cfbcad2d8073ea4fa2174
tianyoulin <tianyoulin@clubfactory.com>
2018-07-23 14:50:25 +0800

    test module

---
+++ test/src/main/java/org/liquid/test/concurrent/RunTrace.java
@@ -0,0 +1,77 @@
+package org.liquid.test.concurrent;
+
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 使用 {@link #get()} 获取运行轨迹，并与其他运行轨迹对比，可获得以下信息：
+ * <ul>
+ *     <li>运行先后顺序</li>
+ *     <li>运行线程</li>
+ * </ul>
+ *
+ * @author linckye 2018-07-21
+ */
+@ToString
+public class RunTrace {
+
+    /**
+     * 运行线程 id
+     */
+    private Long runThreadId;
+
+    /**
+     * 运行先后顺序计数
+     */
+    private Long runOrderCount;
+
+    /**
+     * 运行先后顺序计数器
+     */
+    private static final AtomicLong runOrderCounter = new AtomicLong();
+
+    /**
+     * 是否运行在其之前
+     */
+    public boolean isRunBefore(RunTrace... runTraces) {
+        return Arrays.stream(runTraces)
+                .allMatch(runTrace -> this.runOrderCount < runTrace.runOrderCount);
+    }
+
+    /**
+     * 是否运行在其之后
+     */
+    public boolean isRunAfter(RunTrace... runTraces) {
+        return !isRunBefore(runTraces);
+    }
+
+    /**
+     * 是否运行在同一线程
+     */
+    public boolean isRunOnTheSameThread(RunTrace... runTraces) {
+        return Arrays.stream(runTraces)
+                .allMatch(runTrace -> this.runThreadId.equals(runTrace.runThreadId));
+    }
+
+    /**
+     * 是否运行在不同线程
+     */
+    public boolean isRunOnTheDifferentThread(RunTrace... runTraces) {
+        return !isRunOnTheSameThread(runTraces);
+    }
+
+    /**
+     * 获取当前运行轨迹
+     */
+    public static RunTrace get() {
+        RunTrace runTrace = new RunTrace();
+        runTrace.runThreadId = Thread.currentThread().getId();
+        runTrace.runOrderCount = runOrderCounter.incrementAndGet();
+        return runTrace;
+    }
+
+    private RunTrace() {}
+
+}
