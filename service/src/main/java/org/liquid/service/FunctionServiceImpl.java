318d41da33b3ec1c70e6f60e3fa3113ea383897c
linckye <linckye@gmail.com>
2018-07-27 02:48:50 +0800

    fix depedencies

--- service/src/main/java/org/liquid/service/FunctionServiceImpl.java
+++ service/src/main/java/org/liquid/service/FunctionServiceImpl.java
@@ -4,14 +4,12 @@ import org.liquid.client.FunctionService;
 import org.liquid.client.model.CallFunctionRequest;
 import org.liquid.client.model.CallFunctionResponse;
 import org.liquid.client.response.Response;
-import org.springframework.stereotype.Service;
 
 /**
  * 函数相关服务实现
  *
  * @author linckye 2018-05-16
  */
-@Service
 public class FunctionServiceImpl implements FunctionService {
 
     @Override
