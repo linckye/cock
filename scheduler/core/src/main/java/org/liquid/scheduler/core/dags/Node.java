93233d5eb153191d67e943902c3ff38fd0b6e203
linckye <linckye@gmail.com>
2019-03-24 00:16:12 +0800

    f

--- scheduler/core/src/main/java/org/liquid/scheduler/core/dags/Node.java
+++ scheduler/core/src/main/java/org/liquid/scheduler/core/dags/Node.java
@@ -1,10 +1,25 @@
 package org.liquid.scheduler.core.dags;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.nashorn.internal.ir.ObjectNode;
+import jdk.nashorn.internal.runtime.regexp.joni.constants.NodeStatus;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
 /**
  * {@code Dag} 中的节点，代表作业。
  *
  * @author linckye 2018-07-16
  */
 public interface Node {
+
+    Node downNode(Node node);
+
+    NodeStatus nodeStatus();
+
+
 
 }
